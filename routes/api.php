41b2ae486d65615f04287ee2235b182fd1a1a797
Yehezkiel Imannuel <91413725+yehezkiel0@users.noreply.github.com>
2025-04-11 21:43:46 +0700

    feat: add middleware and api key for secure endpoint

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/city/{city:slug}', [CityController::class, 'show']);
-Route::apiResource('/cities', CityController::class);
+Route::middleware('api_key')->group(function () {
+    Route::get('/city/{city:slug}', [CityController::class, 'show']);
+    Route::apiResource('/cities', CityController::class);
 
-Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
-Route::apiResource('/offices', OfficeSpaceController::class);
+    Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
+    Route::apiResource('/offices', OfficeSpaceController::class);
 
-Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);
-Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
+    Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);
+    Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
+});
