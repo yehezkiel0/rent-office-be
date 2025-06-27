289ae94c84398702e9f174a20cab235edbc12d47
Yehezkiel Imannuel <91413725+yehezkiel0@users.noreply.github.com>
2025-06-28 02:03:30 +0700

    feat: make resource for booking transaction, develop create booking submit

--- app/Http/Controllers/Api/BookingTransactionController.php
+++ app/Http/Controllers/Api/BookingTransactionController.php
@@ -3,9 +3,29 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
+use App\Http\Resources\Api\BookingTransactionResource;
+use App\Models\BookingTransaction;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
-    //
+    public function store(StoreBookingTransactionRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $officeSpace = OfficeSpace::find($validatedData['office_space_id']);
+
+        $validatedData['is_paid'] = false;
+        $validatedData['booking_trx_id'] = BookingTransaction::generateUniqueTrxId();
+        $validatedData['duration'] = $officeSpace->duration;
+        $validatedData['ended_at'] = (new \DateTime($validatedData['started_at']))->modify("+{$officeSpace->duration} days")->format('Y-m-d');
+
+        $bookingTransaction = BookingTransaction::create($validatedData);
+
+        $bookingTransaction->load('officeSpace');
+
+        return new BookingTransactionResource($bookingTransaction);
+    }
 }
